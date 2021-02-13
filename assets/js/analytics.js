0fdb6329229e3379c8f4ff113e41dd8439151637
hschwane <hendrik.schwanekamp@gmx.net>
2021-02-13 20:17:24 +0100

    Update analytics.js

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,7 +6,7 @@
   var gaScript = document.createElement('script');
   gaScript.type = 'text/javascript';
   gaScript.src = 'https://www.googletagmanager.com/gtag/js?id={{ site.google_analytics }}';
-  document.body.appendChild(gaScript);
+  document.head.appendChild(gaScript);
 
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
@@ -20,7 +20,7 @@
   var paScript = document.createElement('script');
   paScript.type = 'text/javascript';
   paScript.src = 'https://cdn.panelbear.com/analytics.js?site={{site.panelbear_analytics}}';
-  document.body.appendChild(paScript);
+  document.head.appendChild(paScript);
   
   window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
   panelbear('config', { site: '{{site.panelbear_analytics}}' });
