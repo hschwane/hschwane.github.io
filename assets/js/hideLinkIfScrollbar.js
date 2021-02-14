a85f8491d8299454f40c4756d480a2f54f822592
hschwane <hendrik.schwanekamp@gmx.net>
2021-02-14 04:01:27 +0100

    Update hideLinkIfScrollbar.js

--- assets/js/hideLinkIfScrollbar.js
+++ assets/js/hideLinkIfScrollbar.js
@@ -2,11 +2,11 @@ let showLinkFunc = function() {
   var elm = document.getElementsByClassName("hide-if-scrollbar");
   if(elm) {
     if ($("body").height() > $(window).height()) {
-      for (i = 0; i < x.length; i++) {
+      for (i = 0; i < elm.length; i++) {
         elm[i].style.visibility = "visible";
       } 
     } else {
-      for (i = 0; i < x.length; i++) {
+      for (i = 0; i < elm.length; i++) {
         elm[i].style.visibility = "hidden";
       } 
     }
